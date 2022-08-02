8fefb63413aa637b99a7a80350aa2d3ddd394f46
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-03 00:28:04 +0700

    final

--- data/main.go
+++ data/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 type Penampung struct {
 	Name string
 	Data []*Data
@@ -9,8 +16,50 @@ type Data struct {
 	Title       string
 	Link        string
 	Description string
+	Content     string
 }
 
 func main() {
+	container := getData()
 
+	//ngambil konten berdasarkan link
+	for _,j := range container.Data{
+		j.Content = getContent(j.Link)
+		fmt.Println(*j)
+	}
+
+	hasil, err := json.Marshal(container)
+	errorHandler(err)
+
+	writeData(hasil)
+
+
+}
+func errorHandler(e error)  {
+	if e!=nil {
+		log.Fatal(e)
+	}
+}
+
+
+func writeData(param []byte)  {
+	err := ioutil.WriteFile("exported.txt", param, 0775)
+	errorHandler(err)
+	
+}
+
+
+func getContent(path string) string  {
+	hasil, err := ioutil.ReadFile(path)
+	errorHandler(err)
+	return string(hasil)
+}
+
+func getData() Penampung {
+	hasil, err := ioutil.ReadFile("./data/data.json")
+	errorHandler(err)
+	var p Penampung
+	jErr:=json.Unmarshal(hasil, &p) // unmarshal : byte => golang
+	errorHandler(jErr)
+	return p
 }
